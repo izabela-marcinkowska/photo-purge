1f439f182bd8deb29fb83937e43e33d9b4c206fc
Devy <izabela.marcinkowska@outlook.com>
2025-05-02 08:43:47 +0200

    Initial Commit

---
+++ Photo Purge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Photo Purge
+//
+//  Created by Izabela Marcinkowska on 2025-05-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Photo Purge/Photo_PurgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_PurgeApp.swift
+//  Photo Purge
+//
+//  Created by Izabela Marcinkowska on 2025-05-02.
+//
+
+import SwiftUI
+
+@main
+struct Photo_PurgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
